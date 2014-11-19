3348471a7691ec902c7a5a14af4fe4aa4880a35c
jow5 <jow5@case.edu>
2014-11-19 18:05:40 -0500

    Update to remove the resetability of the identity filter.
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/IdentityFilter.java
+++ src/main/filters/IdentityFilter.java
@@ -3,15 +3,10 @@ package main.filters;
 import main.filters.interfaces.Filter;
 import main.filters.interfaces.ResetableFilter;
 
-public class IdentityFilter<T> implements Filter<T,T>, ResetableFilter<T> {
+public class IdentityFilter<T> implements Filter<T,T>{
 	
 	private T currentObj;
 	
-	@Override
-	public void reset(T nextInput) {
-		throw new UnsupportedOperationException("Reset not supported.");
-	}
-
 	@Override
 	public void filter(T input) {
 		currentObj = input;
