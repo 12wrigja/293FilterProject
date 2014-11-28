58e7736d2f3409ed9c13c53f269f25f3b3488f96
jow5 <jow5@case.edu>
2014-11-28 18:34:42 -0500

    Update to add javadoc for the exceptions
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/exceptions/BufferException.java
+++ src/main/filters/exceptions/BufferException.java
@@ -1,7 +1,16 @@
 package main.filters.exceptions;
 
+/**The BufferException is a class that represents any errors thrown by the {@link main.utils.Buffer Buffer} class.
+ * @author james
+ *
+ */
 public class BufferException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
+	/**Constructor for a new exception
+	 * @param message
+	 */
 	public BufferException(String message) {
 		super(message);
 	}
--- src/main/filters/exceptions/ResetNotSupportedException.java
+++ src/main/filters/exceptions/ResetNotSupportedException.java
@@ -1,6 +1,19 @@
 package main.filters.exceptions;
 
+/**
+ * The ResetNotSupportedException should be thrown whenever something attempts
+ * to reset a filter that is not resetable. An example of this behaviour is
+ * visible in the {@link main.filters.FilterCascade FilterCascade} class.
+ * 
+ * @author james
+ *
+ */
 public class ResetNotSupportedException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	/**Default Constructor
+	 * @param message
+	 */
 	public ResetNotSupportedException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
