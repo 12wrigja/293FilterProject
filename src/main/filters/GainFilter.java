e65a37452c83b2e299b4a705ac32906bb94e40dc
jow5 <jow5@case.edu>
2014-11-29 18:07:59 -0500

    Update to add javadoc to the GainFilter
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/GainFilter.java
+++ src/main/filters/GainFilter.java
@@ -1,9 +1,16 @@
 package main.filters;
 
+/**The Gain Filter simply returns the item it was passed in, scaled by the scaling value it was originally passed.
+ * @author james
+ *
+ */
 public class GainFilter extends FIRFilter {
 
 	private Double gainVal;
 	
+	/**Constructor for a gain filter. Accepts a double by which to scale the inputs.
+	 * @param gain The double that is multiplied by the inputs to scale them.
+	 */
 	public GainFilter(double gain) {
 		super(0, new double[0]);
 		this.gainVal = new Double(gain);
