a017718d03a8583143bc389bb164868c976a25f2
jow5 <jow5@case.edu>
2014-11-29 18:18:43 -0500

    Update to add javadoc to the binomial filter.
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/BinomialFilter.java
+++ src/main/filters/BinomialFilter.java
@@ -2,18 +2,34 @@ package main.filters;
 
 import org.apache.commons.math3.util.CombinatoricsUtils;
 
-public class BinomialFilter extends FIRFilter{
+/**
+ * The Binomial Filter is a filter that scales all the inputs by binomial
+ * coefficients.
+ * 
+ * @author james
+ *
+ */
+public class BinomialFilter extends FIRFilter {
 
+	/**
+	 * The constructor for the Binomial Filter.
+	 * 
+	 * @param m
+	 *            The number of outputs to store
+	 * @param n
+	 *            The number of inputs to store, which is also used to construct
+	 *            an array of the first n binomial coefficients.
+	 */
 	public BinomialFilter(int m, int n) {
 		super(m, constructBinomailCoefficientArray(n));
 	}
-	
-	private static double[] constructBinomailCoefficientArray(int n){
+
+	private static double[] constructBinomailCoefficientArray(int n) {
 		double[] array = new double[n];
-		for(int i=0; i<n; i++){
+		for (int i = 0; i < n; i++) {
 			array[i] = CombinatoricsUtils.binomialCoefficientDouble(n, i);
 		}
 		return array;
 	}
-	
+
 }
