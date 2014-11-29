b7250e6fbd32871d8a420cbe219bd9ffe7fb02ad
jow5 <jow5@case.edu>
2014-11-29 18:14:28 -0500

    Update to add javadoc to the FIR Filter.
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/FIRFilter.java
+++ src/main/filters/FIRFilter.java
@@ -1,7 +1,23 @@
 package main.filters;
 
+/**
+ * A FIR Filter is an extension to a Scalar Linear Filter that scales all the
+ * outputs stored by 0, and scales all the inputs by whatever value is provided.
+ * 
+ * @author james
+ *
+ */
 public class FIRFilter extends ScalarLinearFilter {
 
+	/**
+	 * The constructor for a FIR Filter.
+	 * 
+	 * @param m
+	 *            The number of outputs to store
+	 * @param b
+	 *            The scaling array for the inputs. For more information on what
+	 *            this does, see the {@link ScalarLinearFilter}
+	 */
 	public FIRFilter(int m, double[] b) {
 		super(new double[m], b);
 	}
