f881e09b45cafed0f64114ebedb33884f78a1647
jow5 <jow5@case.edu>
2014-11-29 17:48:09 -0500

    Update to add javadoc to the IdentityFilter
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/IdentityFilter.java
+++ src/main/filters/IdentityFilter.java
@@ -2,6 +2,11 @@ package main.filters;
 
 import main.filters.interfaces.Filter;
 
+/**The identity filter is a basic filter that simply returns whatever is handed to it.
+ * @author james
+ *
+ * @param <T> The type of object that this filter works on.
+ */
 public class IdentityFilter<T> implements Filter<T,T>{
 	
 	private T currentObj;
