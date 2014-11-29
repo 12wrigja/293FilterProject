e267ea47194f7efc96094dabccb32a5568b25134
jow5 <jow5@case.edu>
2014-11-29 17:42:48 -0500

    Update to add javadoc to the MinN and MaxN filters.
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/MaxNFilter.java
+++ src/main/filters/MaxNFilter.java
@@ -2,6 +2,15 @@ package main.filters;
 
 import java.util.Comparator;
 
+/**
+ * The MaxN filter computes the maximum of the last n items that it has been
+ * passed as input.
+ * 
+ * @author james
+ *
+ * @param <T>
+ *            The type of object that the filter operates on.
+ */
 public class MaxNFilter<T> extends ExtremesNFilter<T> {
 
 	private MaxNFilter(int size) {
@@ -12,10 +21,37 @@ public class MaxNFilter<T> extends ExtremesNFilter<T> {
 		super(FilterMode.MAX, size, comparator);
 	}
 
+	/**
+	 * Constructor method for a maximum n filter that does not use a comparator
+	 * to compare objects. Requires that the type of the object that the filter
+	 * operates on implements Comparable.
+	 * 
+	 * @param <T>
+	 *            The type of object that the filter operates on. Must implement
+	 *            comparable.
+	 * @param size
+	 *            The number of objects to remember and compare against.
+	 * @return Returns an instance of the MaxNFilter constructed with those
+	 *         parameters
+	 */
 	public static <T extends Comparable<T>> MaxNFilter<T> newInstance(int size){
 		return new MaxNFilter<>(size);
 	}
 	
+	/**
+	 * Constructor method for the maximum n filter that requires a comparator to
+	 * compare objects. The objects it operates on can be any type so long as
+	 * the comparator can compare them.
+	 * 
+	 * @param <T>
+	 *            The object type for the filter. Can be anything.
+	 * @param size
+	 *            The number of objects to remember and compare against
+	 * @param comparator
+	 *            The comparator that is used to compare the objects.
+	 * @return Returns an instance of the MaxNFilter constructed with those
+	 *         parameters
+	 */
 	public static <T> MaxNFilter<T> newComparatorInstance(int size, Comparator<T> comparator){
 		return new MaxNFilter<>(size, comparator);
 	}
--- src/main/filters/MinNFilter.java
+++ src/main/filters/MinNFilter.java
@@ -2,22 +2,59 @@ package main.filters;
 
 import java.util.Comparator;
 
+/**
+ * The MinN filter computes the minimum of the last n items that it has been
+ * passed as input.
+ * 
+ * @author james
+ *
+ * @param <T>
+ *            The type of object that the filter operates on.
+ */
 public class MinNFilter<T> extends ExtremesNFilter<T> {
 
 	private MinNFilter(int size) {
 		super(FilterMode.MIN, size);
 	}
-	
+
 	private MinNFilter(int size, Comparator<T> comparator) {
 		super(FilterMode.MIN, size, comparator);
 	}
 
-	public static <T extends Comparable<T>> MinNFilter<T> newInstance(int size){
+	/**
+	 * Constructor method for a minimum n filter that does not use a comparator
+	 * to compare objects. Requires that the type of the object that the filter
+	 * operates on implements Comparable.
+	 * 
+	 * @param <T>
+	 *            The type of object that the filter operates on. Must implement
+	 *            comparable.
+	 * @param size
+	 *            The number of objects to remember and compare against.
+	 * @return Returns an instance of the MinNFilter constructed with those
+	 *         parameters
+	 */
+	public static <T extends Comparable<T>> MinNFilter<T> newInstance(int size) {
 		return new MinNFilter<>(size);
 	}
-	
-	public static <T> MinNFilter<T> newComparatorInstance(int size, Comparator<T> comparator){
+
+	/**
+	 * Constructor method for the minimum n filter that requires a comparator to
+	 * compare objects. The objects it operates on can be any type so long as
+	 * the comparator can compare them.
+	 * 
+	 * @param <T>
+	 *            The object type for the filter. Can be anything.
+	 * @param size
+	 *            The number of objects to remember and compare against
+	 * @param comparator
+	 *            The comparator that is used to compare the objects.
+	 * @return Returns an instance of the MinNFilter constructed with those
+	 *         parameters
+	 */
+	public static <T> MinNFilter<T> newComparatorInstance(int size,
+			Comparator<T> comparator) {
 		return new MinNFilter<>(size, comparator);
 	}
-	
+
 }
