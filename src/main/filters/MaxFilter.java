b07bbdf80f9510138c4ade18136188fe4b9f00d8
jow5 <jow5@case.edu>
2014-11-29 17:25:56 -0500

    Update to add javadoc to the Min and Max filters.
    Signed-off-by: jow5 <jow5@case.edu>

--- src/main/filters/MaxFilter.java
+++ src/main/filters/MaxFilter.java
@@ -2,6 +2,14 @@ package main.filters;
 
 import java.util.Comparator;
 
+/**
+ * The MaxFilter is a filter that computes the maximum of the elements that have
+ * been given to it since it was last reset.
+ * 
+ * @author james
+ *
+ * @param <T> The type of object that the filter operates on 
+ */
 public class MaxFilter<T> extends ExtremesFilter<T>{
 	
 	private MaxFilter(){
@@ -12,11 +20,30 @@ public class MaxFilter<T> extends ExtremesFilter<T>{
 		super(FilterMode.MAX,comparator);
 	}
 	
+	/**
+	 * Constructor method for a maximum filter that uses a comparator to compare
+	 * objects to find their maximum.
+	 * 
+	 * @param <T>
+	 *            The object type for the filter
+	 * @param comparator
+	 *            The comparator for the filter.
+	 * @return Returns an instance of the maximum filter created using that
+	 *         specification
+	 */
 	public static <T> MaxFilter<T> newComparatorInstance(Comparator<T> comparator){
 		return new MaxFilter<T>(comparator);
 	}
 	
-	public static <T extends Comparable> MaxFilter<T> newInstance(){
+	/**
+	 * Constructor method for the maximum filter that does not require a
+	 * comparator to compare objects. Instead requires that the object type
+	 * implements the Comparable interface.
+	 * 
+	 * @param <T> The object type for the filter. Must implement Comparable.
+	 * @return Returns an instance of a maximum filter created.
+	 */
+	public static <T extends Comparable<T>> MaxFilter<T> newInstance(){
 			return new MaxFilter<T>();
 	}
 }
--- src/main/filters/MinFilter.java
+++ src/main/filters/MinFilter.java
@@ -2,21 +2,49 @@ package main.filters;
 
 import java.util.Comparator;
 
-public class MinFilter<T> extends ExtremesFilter<T>{
-	
-	private MinFilter(){
+/**
+ * The MinFilter is a filter that computes the minimum of the elements that have
+ * been given to it since it was last reset.
+ * 
+ * @author james
+ *
+ * @param <T> The type of object that the filter operates on.
+ */
+public class MinFilter<T> extends ExtremesFilter<T> {
+
+	private MinFilter() {
 		super(FilterMode.MIN);
 	}
-	
-	private MinFilter(Comparator<T> comparator){
-		super(FilterMode.MIN,comparator);
+
+	private MinFilter(Comparator<T> comparator) {
+		super(FilterMode.MIN, comparator);
 	}
-	
-	public static <T> MinFilter<T> newComparatorInstance(Comparator<T> comparator){
+
+	/**
+	 * Constructor method for a minimum filter that uses a comparator to compare
+	 * objects to find their minimum.
+	 * 
+	 * @param <T>
+	 *            The object type for the filter
+	 * @param comparator
+	 *            The comparator for the filter.
+	 * @return Returns an instance of the minimum filter created using that
+	 *         specification
+	 */
+	public static <T> MinFilter<T> newComparatorInstance(
+			Comparator<T> comparator) {
 		return new MinFilter<>(comparator);
 	}
-	
-	public static <T extends Comparable<T>> MinFilter<T> newInstance(){
-			return new MinFilter<>();
+
+	/**
+	 * Constructor method for the minimum filter that does not require a
+	 * comparator to compare objects. Instead requires that the object type
+	 * implements the Comparable interface.
+	 * 
+	 * @param <T> The object type for the filter. Must implement Comparable.
+	 * @return Returns an instance of a minimum filter created.
+	 */
+	public static <T extends Comparable<T>> MinFilter<T> newInstance() {
+		return new MinFilter<>();
 	}
 }
